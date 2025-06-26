54ee2a5722d2dcb57ea3343c9594cd52141d2623
zavodovski <lqhheart@gmail.com>
2025-06-26 14:28:08 +0000

    Split out shareable parts

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use std::io::{StdoutLock, Write};
+
+use anyhow::{Context, bail};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message<Payload> {
+    pub src: String,
+    #[serde(rename = "dest")]
+    pub dst: String,
+    pub body: Body<Payload>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Body<Payload> {
+    #[serde(rename = "msg_id")]
+    pub id: Option<usize>,
+    pub in_reply_to: Option<usize>,
+    #[serde(flatten)]
+    pub payload: Payload,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub struct Init {
+    pub node_id: String,
+    pub node_ids: Vec<String>,
+}
+
+
+pub trait Node<Payload> {
+    fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()>;
+
+}
+
+pub fn main_loop<S, Payload>(mut state: S) -> anyhow::Result<()> 
+where S: Node<Payload>, 
+      Payload: DeserializeOwned 
+{
+    let stdin = std::io::stdin().lock();
+    let mut stdout = std::io::stdout().lock();
+
+    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message<Payload>>();
+
+    for input in inputs {
+        let input = input.context("Maelstrom input from STDIN could not be deserialized")?;
+        state
+            .step(input, &mut stdout)
+            .context("Node step function failed")?
+    }
+
+    Ok(())
+}
