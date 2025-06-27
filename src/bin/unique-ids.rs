723ee0a8907db04e74ff61d722a488d22ad20038
zavodovski <lqhheart@gmail.com>
2025-06-27 14:17:33 +0000

    unique id generation

--- src/bin/unique-ids.rs
+++ src/bin/unique-ids.rs
@@ -13,37 +13,26 @@ enum Payload {
         #[serde(rename = "id")]
         guid: String,
     },
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk,
 }
 
 struct UniqueNode {
+    node: String,
     id: usize,
 }
 
-impl Node<Payload> for UniqueNode {
+impl Node<(), Payload> for UniqueNode {
+    fn from_init(_state: (), _init: Init) -> anyhow::Result<Self> where Self: Sized {
+        Ok(UniqueNode { 
+            node: _init.node_id,
+            id: 1 
+            }
+        )
+    }
+
     fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
-            Payload::Init { .. } => {
-                let reply = Message {
-                    src: input.dst,
-                    dst: input.src,
-                    body: Body {
-                        id: Some(self.id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::InitOk,
-                    },
-                };
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialized response to init")?;
-                output.write_all(b"\n").context("write trailing newline")?;
-                self.id += 1;
-            }
             Payload::Generate { .. } => {
-                let guid = ulid::Ulid::new().to_string();
+                let guid = format!("{}-{}", self.node, self.id);
                 let reply = Message {
                     src: input.dst,
                     dst: input.src,
@@ -57,8 +46,7 @@ impl Node<Payload> for UniqueNode {
                     .context("serialized response to init")?;
                 output.write_all(b"\n").context("write trailing newline")?;
                 self.id += 1;
-            }
-            Payload::InitOk { .. } => bail!("InitOk should not be received by the node"),
+            },
             Payload::GenerateOk { .. } => bail!("GenerateOk should not be received by the node"),
         }
 
@@ -67,5 +55,5 @@ impl Node<Payload> for UniqueNode {
 }
 
 fn main() -> anyhow::Result<()> {
-    main_loop(UniqueNode { id: 0 })
+    main_loop::<_, UniqueNode, _>(())
 }
