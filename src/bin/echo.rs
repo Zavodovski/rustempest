d03d42b7079546c1616b17361d7ed92d7eb5146a
zavodovski <lqhheart@gmail.com>
2025-06-27 14:17:04 +0000

    move init logic into lib

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -14,35 +14,19 @@ enum Payload {
     EchoOk {
         echo: String,
     },
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk,
 }
+
 
 struct EchoNode {
     id: usize,
 }
 
-impl Node<Payload> for EchoNode {
+impl Node<(), Payload> for EchoNode {
+    fn from_init(_state: (), _init: Init) -> anyhow::Result<Self> where Self: Sized {
+        Ok(EchoNode { id: 1 })
+    }
     fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
-            Payload::Init { .. } => {
-                let reply = Message {
-                    src: input.dst,
-                    dst: input.src,
-                    body: Body {
-                        id: Some(self.id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::InitOk,
-                    },
-                };
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialized response to init")?;
-                output.write_all(b"\n").context("write trailing newline")?;
-                self.id += 1;
-            }
             Payload::Echo { echo } => {
                 let reply = Message {
                     src: input.dst,
@@ -58,7 +42,6 @@ impl Node<Payload> for EchoNode {
                 output.write_all(b"\n").context("write trailing newline")?;
                 self.id += 1;
             }
-            Payload::InitOk { .. } => bail!("InitOk should not be received by the node"),
             Payload::EchoOk { .. } => {}
         }
 
@@ -67,5 +50,5 @@ impl Node<Payload> for EchoNode {
 }
 
 fn main() -> anyhow::Result<()> {
-    main_loop(EchoNode { id: 0 })
+    main_loop::<_, EchoNode, _>(())
 }
